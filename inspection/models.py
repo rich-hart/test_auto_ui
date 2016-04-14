43c665f0c6aa0a1d0ae7f32a4aadf3eade17a553
rich-hart <rich.hart@rice.edu>
2016-04-14 16:37:07 -0500

    adding models

--- inspection/models.py
+++ inspection/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class PDFs(models.Model):
-    pdf = models.FileField(upload_to='pdfs/')
+class PDF(models.Model):
+    pdf_file = models.FileField(upload_to='pdfs/')
 
 class CVTest(models.Model):
-    pdf_a = models.ForeignKey('PDFs')
-    pdf_b = models.ForeignKey('PDFs')
-    options = models.CharField()
-    results = TextField()
+    pdf_a = models.ForeignKey('PDF',related_name='pdf_a')
+    pdf_b = models.ForeignKey('PDF',related_name='pdf_b')
+    options = models.CharField(max_length=255)
+    results = models.TextField()
 
--- inspection/tests.py
+++ inspection/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+import inspection
+import os
 
-# Create your tests here.
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+TEST_PDF = 'A.pdf'
+class Inspection(TestCase):
+    def test_models(self):
+        pdf = inspection.models.PDF(pdf_file=os.path.join(TEST_DIR,TEST_PDF))
+        pdf.save()
+        cv_test = inspection.models.CVTest(pdf_a=pdf,pdf_b=pdf,options='',results='')    
+        cv_test.save()
+
+
