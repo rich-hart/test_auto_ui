c3865f44909e3f1bb9769b81687bb728f8eff28d
rich-hart <rich.hart@rice.edu>
2016-04-14 16:39:31 -0500

    add REST api to urls

--- test_auto_ui/urls.py
+++ test_auto_ui/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from api import urls as api_urls
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(api_urls)),
 ]
