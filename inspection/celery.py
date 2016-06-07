823761691f70046baafa027509bf34d2a010aae9
rich-hart <rich.hart@rice.edu>
2016-06-07 16:58:02 -0500

    whitespace fix, typo fix, delete unused file

--- inspection/celery.py
+++ inspection/celery.py
@@ -2,7 +2,9 @@ from __future__ import absolute_import
 
 from celery import Celery
 from django.conf import settings
+
 CELERY_RESULT_BACKEND = 'db+{TYPE}://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}'.format(**settings.DATABASES['default'])
+
 app = Celery('inspection',
              broker='django://',
              backend=CELERY_RESULT_BACKEND,
