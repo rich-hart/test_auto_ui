2abc4febc456ae0c0c0fa8c60c4af3831566e447
rich-hart <rich.hart@rice.edu>
2016-06-08 19:39:49 +0000

    add dummy task

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from inspection.models import PDF, CVTest, Option
 from .serializers import PDFSerializer, CVTestSerializer, OptionSerializer
+from inspection.tasks import inspect
 
 class PDFViewSet(viewsets.ModelViewSet):
     queryset = PDF.objects.all()
@@ -16,4 +17,10 @@ class CVTestViewSet(viewsets.ModelViewSet):
     serializer_class = CVTestSerializer
 
     def perform_create(self, serializer):
+        pdf_a_url = self.request.data['pdf_a']
+        pdf_b_url = self.request.data['pdf_b']
+        options = self.request.data['options']
+        inspect.delay(pdf_a_url,
+                      pdf_b_url,
+                      options)
         serializer.save()
--- inspection/tasks.py
+++ inspection/tasks.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import
-
+import subprocess
 from inspection.celery import app
 
+import urllib.request
 
 @app.task
 def add(x, y):
@@ -16,3 +17,8 @@ def mul(x, y):
 @app.task
 def xsum(numbers):
     return sum(numbers)
+
+@app.task
+def inspect(pdf_a_url, pdf_b_url, options):
+    return "Not implemented"    
+    
