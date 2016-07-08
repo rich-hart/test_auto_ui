e74f27c56406ca6d191f4bb28187d1f143a885d9
rich-hart <rich.hart@rice.edu>
2016-07-08 11:30:40 -0500

    add user login

--- test_auto_ui/urls.py
+++ test_auto_ui/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(api_urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
