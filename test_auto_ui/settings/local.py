00e377465e2a9ea42f5e6a945ab9fc178ed3c06e
rich-hart <rich.hart@rice.edu>
2016-10-02 14:35:11 -0500

    update settings

---
+++ test_auto_ui/settings/local.py
@@ -0,0 +1,16 @@
+import os
+import urlparse
+
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,,
+        'TYPE': 'postgres',
+    }
+}
