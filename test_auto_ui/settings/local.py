866960945d7294c7fb8798e8d9a7ba167dd9eab6
rich-hart <rich.hart@rice.edu>
2016-10-03 01:10:49 -0500

    update

--- test_auto_ui/settings/local.py
+++ test_auto_ui/settings/local.py
@@ -3,17 +3,5 @@ import dj_database_url
 
 DEBUG = True
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 DATABASES= { 'default': dj_database_url.config()}
 
