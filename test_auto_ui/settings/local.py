cad40d20dda9250e2727f938f21cdda86b12edad
rich-hart <rich.hart@rice.edu>
2016-10-03 01:16:35 -0500

    update

--- test_auto_ui/settings/local.py
+++ test_auto_ui/settings/local.py
@@ -2,6 +2,7 @@ import os
 import dj_database_url
 
 DEBUG = True
-
-DATABASES= { 'default': dj_database_url.config()}
+db_env = dj_database_url.config()
+db_env['TYPE'] = 'postgres' 
+DATABASES= { 'default': db_env}
 
