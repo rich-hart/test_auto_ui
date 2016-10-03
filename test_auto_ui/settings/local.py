99cdfaad23692489164c5624bf56af6118d51bd2
rich-hart <rich.hart@rice.edu>
2016-10-02 22:05:07 -0500

    update

--- test_auto_ui/settings/local.py
+++ test_auto_ui/settings/local.py
@@ -3,8 +3,17 @@ import urlparse
 
 DEBUG = False
 
-STATIC_ROOT = '/app/test_auto_ui/static'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
