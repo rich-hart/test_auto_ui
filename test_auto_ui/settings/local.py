88ce881c924a3d6f110938a641870bbb1ae6e018
rich-hart <rich.hart@rice.edu>
2016-10-03 01:01:23 -0500

    update settings

--- test_auto_ui/settings/local.py
+++ test_auto_ui/settings/local.py
@@ -15,6 +15,5 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default']=dj_database_url.config()
 
